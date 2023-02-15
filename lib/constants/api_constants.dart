a5a28f8456e56958b5cf66bd3cf2714f9e7ff99b
Luiz Montuani <luizmontuani@ioasys.com.br>
2023-02-15 19:49:08 -0300

    fix constant

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,2 +1,2 @@
-String baseUrl = 'https://api.openai.com/v1/';
-String apiKey = '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';
+String baseUrl = 'https://api.openai.com/v1';
+String apiKey = 'here you have to put your api key';
